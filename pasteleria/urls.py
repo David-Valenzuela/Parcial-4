d3dbd406dcbbc494a84fd33d556d0ccf0b65b4dd
David Valenzuela <dav.valenzuelat@duocuc.cl>
2021-06-30 22:19:44 -0400

    Agregar carpeta static con archivos css, js y img

--- pasteleria/urls.py
+++ pasteleria/urls.py
@@ -4,4 +4,8 @@ from . import views
 app_name = 'pasteleria'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('agregar_cliente', views.agregar_cliente, name='agregar_cliente'),
+    path('registrar_datos', views.registrar_datos, name='registrar_datos'),
+    
+
 ]
